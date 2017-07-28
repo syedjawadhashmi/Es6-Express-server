3bac42cccb6061c5acea119914bf2c23e7bd656b
syedjawadhashmi <sjawad143@yahoo.com>
2017-07-29 00:53:04 +0500

    fixed

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import path from 'path';
 import express from 'express';
-
+import cors from 'cors';
 export default app => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +10,7 @@ export default app => {
 //app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 // app.use(favicon(path.join(__dirname, '/../public', 'favicon.ico')));
-// app.use(cors());
+ app.use(cors());
 // app.use(helmet());
 // app.use(compression());
 // app.use(morgan('dev'));
